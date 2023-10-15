aed659d81056a6acf970de299e7ba02f1e151a10
zjbfshuimen <134823604+zjbfshuimen@users.noreply.github.com>
2023-10-15 18:16:57 +0800

    style change

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,117 +1,125 @@
-public class ArrayDeque <Item> {
-   private int size;
-   private int begin, end;
-   private Item[] items;
-   private double radio;
+public class ArrayDeque<T> {
 
-   public ArrayDeque(){
-       items = (Item[]) new Object[8];
-       size = 0;
-       begin = 0;
-       end = 0;
+    private int size;
+    private int begin, end;
+    private T[] items;
+    private double radio;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        begin = 0;
+        end = 0;
     }
 
-    public void addFirst(Item x){
-        size ++;
-        if(begin == end){
+    public void addFirst(T x) {
+        size++;
+        if (begin == end) {
             items[begin] = x;
         }
-        if(((begin - 1 + items.length) % items.length) == end) {
+        if (((begin - 1 + items.length) % items.length) == end) {
             recap();
         }
         begin = (begin - 1 + items.length) % items.length;
         items[begin] = x;
     }
-    public void addLast(Item x){
-       size ++;
-       if(begin == end){
-           items[end] = x;
-       }
-       if((end + 1) % items.length == begin) {
-           recap();
-       }
-       end = (end + 1) % items.length;
-       items[end] = x;
+    public void addLast(T x) {
+        size++;
+        if (begin == end) {
+            items[end] = x;
+        }
+        if ((end + 1) % items.length == begin) {
+            recap();
+        }
+        end = (end + 1) % items.length;
+        items[end] = x;
 
     }
-    public boolean isEmpty(){
-       if(size == 0){
-           return true;
-       }
-       return false;
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printDeque(){
-       if(size != 0) {
-           int i;
-           for (i = begin; i != end; i = (i + 1) % items.length) {
-               System.out.print(items[i] + " ");
-           }
-           System.out.print(items[i] + " ");
-       }
+    public void printDeque() {
+        if (size != 0) {
+            int i;
+            for (i = begin; i != end; i = (i + 1) % items.length) {
+                System.out.print(items[i] + " ");
+            }
+            System.out.print(items[i] + " ");
+        }
 
 
     }
-    public Item removeFirst(){
-       if(size == 0){
-           return null;
-       }
-       size --;
-       Item x = items[begin];
-       items[begin] = null;
-       begin = (begin + 1) % items.length;
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        T x = items[begin];
+        items[begin] = null;
+        begin = (begin + 1) % items.length;
 
-       radio = (double)size / items.length;
-       if( radio <= 0.25) {
-           shrink();
-       }
+        radio = (double) size / items.length;
+        if (radio == 0) {
+            return null;
+        }
+        if (radio <= 0.25) {
+            shrink();
+        }
 
-       return x;
+        return x;
     }
-    public Item removeLast(){
-       if(size == 0){
-           return null;
-       }
-       size --;
-       Item x = items[end];
-       items[end] = null;
-       end = (end - 1 + items.length) % items.length;
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        T x = items[end];
+        items[end] = null;
+        end = (end - 1 + items.length) % items.length;
 
-       radio = (double) size / items.length;
-       if(radio <= 0.25){
-           shrink();
-       }
-       return x;
+        radio = (double) size / items.length;
+        if (radio == 0) {
+            return null;
+        }
+        if (radio <= 0.25) {
+            shrink();
+        }
+        return x;
     }
-    public Item get(int index){
-       int i = (begin + index) % items.length;
+    public T get(int index) {
+        int i = (begin + index) % items.length;
         return items[i];
     }
-    public void recap(){
-       Item[] newItems = (Item[]) new Object[items.length * 2];
-       if(begin <= end) {
-           System.arraycopy(items, 0, newItems, 0, items.length);
-           items = newItems;
-           return;
-       }
-       System.arraycopy(items, 0, newItems, 0, end + 1);
-       int rightLength = items.length - begin;
-       System.arraycopy(items, begin, newItems, newItems.length - rightLength - 1, rightLength);
+
+    public void recap() {
+        T[] newItems = (T[]) new Object[items.length * 2];
+        if (begin <= end) {
+            System.arraycopy(items, 0, newItems, 0, items.length);
+            items = newItems;
+            return;
+        }
+        System.arraycopy(items, 0, newItems, 0, end + 1);
+        int rightLength = items.length - begin;
+        System.arraycopy(items, begin, newItems, newItems.length - rightLength - 1, rightLength);
     }
 
-    public void shrink(){
-        Item[] newItems = (Item[]) new Object[items.length/2];
-        if(begin <= end){
+    public void shrink() {
+        T[] newItems = (T[]) new Object[items.length / 2];
+        if (begin <= end) {
             System.arraycopy(items, begin, newItems, 0, size);
             items = newItems;
             return;
         }
         System.arraycopy(items, begin, newItems, 0, items.length - begin);
-        System.arraycopy(items, end, newItems, items.length - begin, end + 1 );
+        System.arraycopy(items, end, newItems, items.length - begin, end + 1);
         items = newItems;
     }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,20 +1,20 @@
-public class LinkedListDeque<Item> {
-    public class Node{
-        Item item;
+public class LinkedListDeque<T> {
+    private class Node {
+        T item;
         Node next;
         Node prev;
-        public Node(Item x) {
-           item = x;
-           prev = null;
-           next = null;
+        public Node(T x) {
+            item = x;
+            prev = null;
+            next = null;
         }
-        public Node(Item x, Node p, Node r) {
+        public Node(T x, Node p, Node r) {
             item = x;
             prev = p;
             next = r;
         }
-        public Item getRecursiveHelper(int i){
-            if(i == 0) {
+        public T getRecursiveHelper(int i) {
+            if (i == 0) {
                 return this.item;
             }
             return this.next.getRecursiveHelper(i - 1);
@@ -27,39 +27,32 @@ public class LinkedListDeque<Item> {
     /**
      * default constructor
      */
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         front = new Node(null);
         front.next = front;
         front.prev = front;
         size = 0;
     }
 
-    public LinkedListDeque(Item x) {
-        Node N = new Node(x);
-        front.next = N;
-        front.prev = front.next;
-        N.prev = front;
-        N.next = front;
-        size = 1;
-    }
-    public void addFirst(Item x) {
-        Node nexttoF =front.next;
+
+    public void addFirst(T x) {
+        Node nexttoF = front.next;
         Node N = new Node(x, front, front.next);
         front.next = N;
         nexttoF.prev = N;
         size += 1;
     }
 
-    public void addLast(Item x) {
+    public void addLast(T x) {
         Node prevF = front.prev;
-        Node N = new Node(x, front.prev , front);
+        Node N = new Node(x, front.prev, front);
         front.prev = N;
         prevF.next = N;
         size += 1;
     }
     /** Return true if the List is empty*/
     public boolean isEmpty() {
-        if(size == 0) {
+        if (size == 0) {
             return true;
         }
         return false;
@@ -71,14 +64,14 @@ public class LinkedListDeque<Item> {
 
     public void printDeque() {
         Node p = front.next;
-        while (p != front ) {
+        while (p != front) {
             System.out.print(p.item + " ");
             p = p.next;
         }
     }
 
-    public Item removeFirst() {
-        if(size == 0) {
+    public T removeFirst() {
+        if (size == 0) {
             return null;
         }
         Node toRemove = front.next;
@@ -88,8 +81,8 @@ public class LinkedListDeque<Item> {
         return toRemove.item;
     }
 
-    public Item removeLast() {
-        if(size == 0){
+    public T removeLast() {
+        if (size == 0) {
             return null;
         }
         Node toRemove = front.prev;
@@ -99,18 +92,18 @@ public class LinkedListDeque<Item> {
         return toRemove.item;
     }
 
-    public Item get(int i) {
-        if(i >= size) {
+    public T get(int i) {
+        if (i >= size) {
             return null;
         }
         Node p = front.next;
-        for(int k = 0; k < i; k++) {
+        for (int k = 0; k < i; k++) {
             p = p.next;
         }
         return p.item;
     }
-    public Item getRecursive(int index) {
-        if(index >= size) {
+    public T getRecursive(int index) {
+        if (index >= size) {
             return null;
         }
         return front.next.getRecursiveHelper(index);
