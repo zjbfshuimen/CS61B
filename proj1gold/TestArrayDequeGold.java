0c2c4884459202ead9552505ff1b57977b98153a
zjbfshuimen <134823604+zjbfshuimen@users.noreply.github.com>
2023-10-21 22:55:15 +0800

    add an ArrayList to remember the used method to cause error

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,10 +1,19 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestArrayDequeGold {
     private final int outtime = 3000;
     @Test(timeout = outtime)
     public void testfirst() {
+        /*
+         * 1 for addFirst 2 for addLast
+         * 3 for removeFirst     4 for removeLast
+         * 5 for size()
+         */
+        List<Integer> usedMethod = new ArrayList<>();
         int methodNum = 5;
         int[] randomBox = new int[methodNum];
         ArrayDequeSolution<Integer> items = new ArrayDequeSolution<>();
@@ -13,38 +22,71 @@ public class TestArrayDequeGold {
             randomBox[i] = i;
             items.addFirst(i);
             actems.addFirst(i);
+            usedMethod.add(1);
         }
+
 
         while (true) {
             int randomNumber = StdRandom.discrete(randomBox);
             switch (randomNumber) {
-                case 0: assertEquals("your ArrayDeque.removeLast is not correct ",
-                        items.removeLast(), actems.removeLast());
+                case 0:
+                    usedMethod.add(4);
+                    assertEquals(errorCausedRank(usedMethod),
+                            items.removeLast(), actems.removeLast());
                     break;
                 case 1:
+                    usedMethod.add(5);
                     if (items.size() != 0 && actems.size() != 0) {
-                        assertEquals("your ArrayDeque.removeFirst is not correct",
-                                items.removeFirst(), actems.removeFirst());
+                        usedMethod.add(1);
+                        assertEquals(errorCausedRank(usedMethod),
+                            items.removeFirst(), actems.removeFirst());
                     }
                     break;
                 case 2:
+                    usedMethod.add(5);
                     assertEquals(items.size(), actems.size());
                     break;
-                case 3:
                 default:
                     int i = StdRandom.discrete(randomBox);
                     items.addFirst(i);
                     items.addLast(i + 1);
+                    usedMethod.add(1);
+                    usedMethod.add(2);
                     actems.addFirst(i);
                     actems.addLast(i + 1);
-                    if (items.size() != 0 && actems.size() != 0) {
-                        assertEquals("After addFirst and addLast, removeFirst(), in which case "
-                                        + "your ArrayDeque.removeFirst is not correct",
-                                items.removeFirst(), actems.removeFirst());
-                    }
 
+                    usedMethod.add(5);
+                    if (items.size() != 0 && actems.size() != 0) {
+                        usedMethod.add(3);
+                        assertEquals(errorCausedRank(usedMethod),
+                            items.removeFirst(), actems.removeFirst());
+                    }
             }
         }
-
     }
+
+    private String errorCausedRank(List<Integer> usedMethod) {
+        String returnString = "";
+        for (Integer a : usedMethod) {
+            switch (a) {
+                case 1:
+                    returnString += "addFirst()\n";
+                    break;
+                case 2:
+                    returnString += "addLast()\n";
+                    break;
+                case 3:
+                    returnString += "removeFirst()\n";
+                    break;
+                case 4:
+                    returnString += "removeLast()\n";
+                    break;
+                default:
+                    returnString += "size()\n";
+                    break;
+            }
+        }
+        return returnString;
+    }
+
 }
