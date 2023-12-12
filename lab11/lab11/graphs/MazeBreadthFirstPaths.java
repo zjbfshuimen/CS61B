9ebd2ab4db6ea489b64566524fdf235bba29b2e5
zjbfshuimen <134823604+zjbfshuimen@users.noreply.github.com>
2023-12-12 20:10:41 +0800

    add lab 11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +11,55 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean find = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> french = new Queue<>();
+        french.enqueue(s);
+        marked[s] = true;
+        announce();
+
+        while (!french.isEmpty()) {
+            int toRemove = french.dequeue();
+
+            if (toRemove == t) {
+                break;
+            }
+
+            for (int w : maze.adj(toRemove)) {
+                if (marked[w]) {
+                    continue;
+                }
+                french.enqueue(w);
+                marked[w] = true;
+                edgeTo[w] = toRemove;
+                distTo[w] = distTo[toRemove] + 1;
+                announce();
+            }
+
+
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +11,68 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s = 0;
+    private Maze maze;
+    private boolean findCycle = false;
+    private int[] nodeTo;
 
     public MazeCycles(Maze m) {
         super(m);
+        nodeTo = new int[m.V()];
+        for (int i = 0; i < edgeTo.length; i++) {
+            nodeTo[i] = edgeTo[i];
+        }
+        maze = m;
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     @Override
     public void solve() {
+        Stack<Integer> french = new Stack<>();
+        french.push(s);
+        announce();
+
+        while (!french.isEmpty() && !findCycle) {
+            int toRemove = french.pop();
+
+            if (!marked[toRemove]) {
+                marked[toRemove] = true;
+                announce();
+
+                for (int w : maze.adj(toRemove)) {
+                    if (!marked[w]) {
+                        french.push(w);
+                        distTo[w] = distTo[toRemove] + 1;
+                        edgeTo[w] = toRemove;
+                        announce();
+                    }
+
+                    if (marked[w] && edgeTo[toRemove] != w) {
+                        findCycle = true;
+                        edgeTo[w] = toRemove;
+                        copyToCycle(w);
+                        announce();
+
+                        distTo = nodeTo;
+                    }
+
+                }
+            }
+
+        }
+
         // TODO: Your code here!
     }
 
     // Helper methods go here
+    private void copyToCycle(int start) {
+        int prev = edgeTo[start];
+        nodeTo[start] = edgeTo[start];
+        while (prev != start) {
+            nodeTo[prev] = edgeTo[prev];
+            prev = edgeTo[prev];
+        }
+    }
 }
 
